77f05d62ec46a3fca1449030db2b399bfca3e914
Shashank <s_shashank@outlook.com>
2020-08-25 22:50:57 +0530

    initial setup

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Firesplash</h1>
+      <p>Upload your beautiful picture.</p>
+    </div>
+  );
+};
+
+export default Title;
